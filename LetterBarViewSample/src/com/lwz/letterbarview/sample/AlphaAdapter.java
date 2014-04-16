f25bade1966c56d12ad25be8892846a1c01e5a9c
lwz <lwz0316@gmail.com>
2014-04-16 11:23:30 +0800

    修改 包名，readme

---
+++ LetterBarViewSample/src/com/lwz/letterbarview/sample/AlphaAdapter.java
@@ -0,0 +1,167 @@
+package com.lwz.letterbarview.sample;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * 字母导航适配器。根据数据源按字母分类，每一个分类间会有该类的字母来分隔。
+ * 
+ * <p><b>NOTE:</b> 数据源会自动重新按字母排序
+ * 
+ * <p> 由于该适配器的实现会同样对字母分隔 Item 进行点击事件的监听，故若要使点击事件只针对源数据，则应使用 {@link OnItemClickWrapperListener}. 例如
+ * <pre>
+listView.setOnItemClickListener(new AlphaAdapter.OnItemClickWrapperListener&ltNews&gt() {
+
+	public void onItemClick(News itemData) {
+		// TODO
+	}
+			
+});
+</pre>
+ * @author Liu Wenzhu
+ *
+ * @param <T> 数据源类型
+ */
+public abstract class AlphaAdapter<T extends AlphaWrapper> extends BaseAdapter {
+	
+	private static final int VIEW_TYPE_ALPHA = 0;
+	private static final int VIEW_TYPE_COMMON = 1;
+	
+	private ArrayList<T> mOriginData;
+	private ArrayList<Object> mMergeData;
+	private LayoutInflater mLayoutInflater;
+	private HashMap<String, Integer> mAlphaMap;
+
+	public AlphaAdapter(Context context, ArrayList<T> data) {
+		mLayoutInflater = LayoutInflater.from(context);
+		mOriginData = data;
+		mergeData();
+	}
+	
+	private void mergeData() {
+		Collections.sort(mOriginData, new Comparator<T>() {
+
+			@Override
+			public int compare(T lhs, T rhs) {
+				return lhs.getAlpha().compareTo(rhs.getAlpha());
+			}
+			
+		});
+		mMergeData = new ArrayList<Object>();
+		mAlphaMap = new HashMap<String, Integer>();
+		final int SIZE = mOriginData.size();
+		String lastAlpha = "";
+		for( int i=0; i<SIZE; i++ ) {
+			T item = mOriginData.get(i);
+			if( !lastAlpha.equals(item.getAlpha().substring(0, 1))) {
+				lastAlpha = item.getAlpha().substring(0, 1);
+				mAlphaMap.put(lastAlpha, mMergeData.size());
+				mMergeData.add(lastAlpha);
+			}
+			mMergeData.add(item);
+		}
+	}
+	
+	public ArrayList<Object> getMergeData() {
+		return mMergeData;
+	}
+	
+	public boolean containsAlpha(String alpha) {
+		return mAlphaMap.containsKey(alpha);
+	}
+	
+	public int getAlphaPosition(String alpha) {
+		return mAlphaMap.get(alpha);
+	}
+
+	@Override
+	public int getCount() {
+		return mMergeData.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mMergeData.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+	
+	@Override
+	public int getItemViewType(int position) {
+		if( mMergeData.get(position) instanceof AlphaWrapper ) {
+			return VIEW_TYPE_COMMON;
+		}
+		return VIEW_TYPE_ALPHA;
+	}
+	
+	@Override
+	public int getViewTypeCount() {
+		return 2;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if( convertView == null ) {
+			switch( getItemViewType(position) ) {
+				case VIEW_TYPE_ALPHA :
+					convertView = mLayoutInflater.inflate(R.layout.item_list_alpha, parent, false);
+					break;
+				default :
+					convertView = mLayoutInflater.inflate(R.layout.item_list_news, parent, false);
+			}
+		}
+		
+		bindData(position, convertView);
+		return convertView;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void bindData(int position, View convertView) {
+		switch (getItemViewType(position)) {
+			case VIEW_TYPE_ALPHA:
+				TextView alphaText = ViewHolder.getView(convertView, R.id.alpha);
+				alphaText.setText(mMergeData.get(position).toString());
+				break;
+			default:
+				bindOriginData(position, convertView, (T)mMergeData.get(position));
+		}
+	}
+	
+	public abstract void bindOriginData(int position, View convertView, T itemData);
+	
+	/**
+	 * 避免 点击字母条响应点击事件。点击事件只针对源数据
+	 * @author lwz
+	 *
+	 * @param <T>
+	 */
+	public static abstract class OnItemClickWrapperListener<T extends AlphaWrapper> implements AdapterView.OnItemClickListener {
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public final void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			AlphaAdapter<?> adapter = (AlphaAdapter<?>)parent.getAdapter();
+			if( adapter.getItemViewType(position) == VIEW_TYPE_ALPHA ) {
+				return;
+			}
+			onItemClick((T)adapter.getMergeData().get(position));
+		}
+		
+		public abstract void onItemClick(T itemData);
+		
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,96 @@
+## Android 字母导航条 (Android Letter Bar View)
+
+实现了类似于 Android 手机中字母导航功能， 支持导航条自定义字母集。
+
+![](screen_shot.png)
+
+##如何使用
+
+请参照 `\LetterBarViewSample` 中的例子来使用。
+
+#####1. 添加 `LetterBarView` 到你想使用的布局中。
+
+注意：若要在 layout 中使用 LetterBarView 自定义的 attr(在例子中定义为 `letterBar`)，则必须在 layout 开头添加  `xmlns:letterBar="http://schemas.android.com/apk/res/[你的包名]`。
+
+下面所示的代码就是完整的示例。
+
+		<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
+			xmlns:letterBar="http://schemas.android.com/apk/res/com.lwz.letterbarview.sample" // 注意这个地方
+		    xmlns:tools="http://schemas.android.com/tools"
+		    android:layout_width="match_parent"
+		    android:layout_height="match_parent"
+		    android:background="@android:color/white" >
+		
+		    <ListView
+		        android:id="@+id/listview"
+		        android:layout_width="match_parent"
+		        android:layout_height="wrap_content"
+		        android:cacheColorHint="#00000000"
+		        android:paddingRight="20dp" />
+		
+		    <com.lwz.letterbarview.lib.LetterBarView
+		        android:id="@+id/letter_bar"
+		        android:layout_width="match_parent"
+		        android:layout_height="match_parent"
+		        android:layout_gravity="right"
+		        android:paddingBottom="10dp"
+		        android:paddingTop="10dp"
+		        letterBar:lbOverlayTextSize="40sp"
+		        letterBar:lbOverlayTextColor="#FFF"
+		        letterBar:lbOverlayBackground="@drawable/bg_overlay"
+		        letterBar:lbLetterBarTextColor="@color/selector_letterbar_text"
+		        letterBar:lbLetterBarBackground="@drawable/bg_letterbar" />
+		</FrameLayout>
+
+若您对使用 自定义的 attr 不明白，可以参照 
+
+- [Android Custom UI With custom Attributes](http://stackoverflow.com/questions/7608464/android-custom-ui-with-custom-attributes "Android - custom UI with custom attributes") - `stackoverflow.com`
+- [Define Custom Attributes](http://developer.android.com/training/custom-views/create-view.html#customattr) - `developer.android.com`
+
+##########2. 自定义的属性 attr
+- lbLetterBarBackground - 导航条的背景 应该使用 图片选择器，可以参照 [Selector requires drawable attribute](http://stackoverflow.com/questions/9997943/selector-requires-drawable-attribute) or [drawable-resource](http://developer.android.com/guide/topics/resources/drawable-resource.html#StateList) 
+- lbLetterBarTextColor - 导航条的字母颜色 应该使用颜色选择器，可以参照 [Android selector & text color](http://stackoverflow.com/questions/1219312/android-selector-text-color/3565624#3565624) or [color list resource](http://developer.android.com/guide/topics/resources/color-list-resource.html)
+- lbOverlayBackground - 选中字母弹出层的背景
+- lbOverlayTextColor - 选中字母弹出层字母的颜色
+- lbOverlayTextSize - 选中字母弹出层字母的大小
+
+#####3. 在代码中使用
+
+就像你使用其他 View 一样简单
+
+	LetterBarView letterBar = (LetterBarView) findViewById(R.id.letter_bar);
+	letterBar.setOnLetterSelectListener(new OnLetterSelectListener() {
+		
+		@Override
+		public void onLetterSelect(String s) {
+			// 用户选中字母后的回调
+			// TODO
+		}
+	});
+
+##开源证书 MIT
+
+	Copyright 2014 Liu wenzhu
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+	
+
+
+
